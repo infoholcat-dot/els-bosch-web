f9a051103f0c5fba839fe0106d8752f2d29a7ba3
roger <rogerbosch4@gmail.com>
2026-07-13 21:54:59 +0200

    Fix: window.supabase client initialization

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
-// Substituir amb les claus del teu projecte Supabase
 const SUPABASE_URL = 'https://eqwhkejwoaarguqaixeq.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxd2hrZWp3b2Fhcmd1cWFpeGVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4MTMzNTQsImV4cCI6MjA5OTM4OTM1NH0.yl2YMaxbLeQAVuwdplN4_ePqSYmNrM_6BSZcBFSqE-g';
 
-const { createClient } = supabaseJs;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
